0352f732bd3cc06587969e45d0fc0eb48ad1bd01
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-08-29 12:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	let newStr = s.trim().split(" ");
+	if(newStr.length > 0) return newStr[0];
+	return " ";
+	
 }
 
 // Do not change the code below
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
